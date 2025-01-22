a55af477c53ec692f3b74c67fc015bb21b7838c5
Sumir Seth <iamsumirseth@gmail.com>
2025-01-22 21:56:07 +0530

    style: comment out background noise CSS for future reference

--- app.vue
+++ app.vue
@@ -67,9 +67,9 @@ onMounted(() => {
 <style>
 @import url('https://rsms.me/inter/inter.css');
 
-.bg-noise {
+/* .bg-noise {
   background-image: url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noise'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.65' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%' height='100%' filter='url(%23noise)'/%3E%3C/svg%3E");
-}
+} */
 
 ::selection {
   @apply bg-indigo-500/20;
